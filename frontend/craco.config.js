21219c582ba70022ef469c74bf420c3c4f79b3b8
kravii <ravi@acceldata.io>
2025-09-18 17:32:52 -0400

    fix frontend

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,13 +1,7 @@
 module.exports = {
   devServer: {
     allowedHosts: 'all',
-    host: '0.0.0.0',
-    port: 3000,
-    client: {
-      webSocketURL: 'ws://172.30.0.2:3000/ws',
-    },
-    headers: {
-      'Access-Control-Allow-Origin': '*',
-    },
-  },
+    // Alternative specific hosts configuration:
+    // allowedHosts: ['localhost', '127.0.0.1', 'odp01.acceldata.dvl'],
+  }
 };
